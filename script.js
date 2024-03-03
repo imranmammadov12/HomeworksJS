d1b5484b4f2962a7ab0e2551c414d6c0f631a33e
imranmammadov12 <Mamed_qf28@itstep.edu.az>
2024-03-03 22:29:35 +0400

    homeworkprototype

--- script.js
+++ script.js
@@ -1,33 +1,11 @@
-const apiUrl = 'https://rickandmortyapi.com/api/character';
-let currentPage = 1;
-
-async function fetchData(url) {
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
-}
-
-async function displayCharacters(url) {
-  const data = await fetchData(url);
-  const characters = data.results.map(character => character.name);
-  console.log(characters);
-}
-
-async function handlePrevClick() {
-  if (currentPage > 1) {
-    currentPage--;
-    const url = `${apiUrl}?page=${currentPage}`;
-    await displayCharacters(url);
+function printPrototypeChain(obj) {
+  console.log(obj);
+  if (Object.getPrototypeOf(obj) === null) {
+    console.log(null);
+    process.exit(); 
   }
+  printPrototypeChain(Object.getPrototypeOf(obj));
 }
 
-async function handleNextClick() {
-  const url = `${apiUrl}?page=${currentPage + 1}`;
-  await displayCharacters(url);
-  currentPage++;
-}
-
-document.getElementById('prev').addEventListener('click', handlePrevClick);
-document.getElementById('next').addEventListener('click', handleNextClick);
-
-displayCharacters(apiUrl);
+const obj = {};
+printPrototypeChain(obj);
